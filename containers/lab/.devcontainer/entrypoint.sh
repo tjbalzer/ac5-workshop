9b3e9684b4249b2c9f7dea8a61397067e2c145e7
tjbalzer <t.j.balzer@gmail.com>
2026-06-08 10:43:37 +0000

    Add labs and Makefiles

--- containers/lab/.devcontainer/entrypoint.sh
+++ containers/lab/.devcontainer/entrypoint.sh
@@ -7,6 +7,18 @@ if [ -z "${CODE_SERVER_BIND_ADDR}" ]; then
 fi
 code-server --bind-addr ${CODE_SERVER_BIND_ADDR} --auth password --disable-telemetry --disable-update-check --disable-workspace-trust "${CONTAINERWSF}" &
 
+# run magic moby script for D-in-D
+/usr/local/share/docker-init.sh
+
+# check if ceos-lab image already present
+if [ -z "$(${CONTAINER_ENGINE} image ls | grep 'arista/ceos')" ]; then
+    docker load < ceos_lab.tar.gz
+    echo "WARNING: cEOS-lab image was successfully loaded."
+fi
+
+# start the lab
+make start
+
 # on Codespaces this will not work correctly
 if ! ${CODESPACES:-false}; then
     # Execute command from docker cli if any.
