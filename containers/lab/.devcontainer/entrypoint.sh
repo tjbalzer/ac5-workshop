b8fc339390917f7fbfd8156305df10f047d856f0
tjbalzer <t.j.balzer@gmail.com>
2026-06-08 09:55:52 +0000

    add containerlab

--- containers/lab/.devcontainer/entrypoint.sh
+++ containers/lab/.devcontainer/entrypoint.sh
@@ -2,6 +2,11 @@
 
 set +e
 
+if [ -z "${CODE_SERVER_BIND_ADDR}" ]; then
+    CODE_SERVER_BIND_ADDR="0.0.0.0:5000"
+fi
+code-server --bind-addr ${CODE_SERVER_BIND_ADDR} --auth password --disable-telemetry --disable-update-check --disable-workspace-trust "${CONTAINERWSF}" &
+
 # on Codespaces this will not work correctly
 if ! ${CODESPACES:-false}; then
     # Execute command from docker cli if any.
